c09da43539d2a02ddd44763bcef4f5621df7f7b0
Fabricio Ryu <fabricio.akashi26@gmail.com>
2026-04-16 16:19:57 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API = "http://localhost:3000";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [editingId, setEditingId] = useState(null); // ✅ AQUI
+
+  const fetchUsers = async () => {
+    const res = await axios.get(`${API}/users`);
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const createUser = async () => {
+    await axios.post(`${API}/users`, { name, email });
+    setName("");
+    setEmail("");
+    fetchUsers();
+  };
+
+  const updateUser = async () => {
+    await axios.put(`${API}/users/${editingId}`, {
+      name,
+      email,
+    });
+
+    setName("");
+    setEmail("");
+    setEditingId(null);
+    fetchUsers();
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`${API}/users/${id}`);
+    fetchUsers();
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>CRUD</h1>
+
+      <input
+        placeholder="Nome"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button onClick={editingId ? updateUser : createUser}>
+        {editingId ? "Atualizar" : "Criar"}
+      </button>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name} - {user.email}
+
+            <button
+              onClick={() => {
+                setName(user.name);
+                setEmail(user.email);
+                setEditingId(user._id);
+              }}
+            >
+              Editar
+            </button>
+
+            <button onClick={() => deleteUser(user._id)}>
+              Excluir
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
